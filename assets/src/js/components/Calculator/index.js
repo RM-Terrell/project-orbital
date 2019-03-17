783272958a712ae1b494a4fa84ef7c0dc02c6c6f
RM-Terrell <terrellsystems@gmail.com>
2019-03-17 15:00:39 -0600

    super basic router nav

---
+++ assets/src/js/components/Calculator/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+      <h1 className="title"></h1>
+    )
+  }
+
+export default Calculator;
--- assets/src/js/containers/App/index.js
+++ assets/src/js/containers/App/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Title from '../../components/Title';
+import Home from '../../components/Home'
+import Calculator from '../../components/Calculator'
 
-class App extends React.Component {
+class App extends Component {
   render () {
-    const text = 'Django + React + Webpack + Babel = Awesome App';
+    const text = 'Project Orbital';
     return (
-      <Title text={text} />
+      <Router>
+        <div>
+          <h2>Welcome to Project Orbital</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/calculator'} className="nav-link">Calculator</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/calculator' component={Calculator} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
